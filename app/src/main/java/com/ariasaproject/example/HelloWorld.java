a02ec92a9a2aa02dba658b360ab14f79f9aed110
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-10-16 16:01:42 +0800

    new

---
+++ app/src/main/java/com/ariasaproject/example/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.ariasaproject.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class HelloWorld extends Activity {
+	@Override
+	public void onCreate(Bundle b) {
+		setContentView(R.layout.activity_main);
+		super.onCreate(b);
+	}
+}
+
+
