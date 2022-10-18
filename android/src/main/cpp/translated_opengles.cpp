65d9b151246ecc6bfb40f63d85bbf9062d9c8fe7
Ariasa_Prop <ikomangwidiadaariasa12@gmail.com>
2022-10-19 05:50:46 +0800

    draw gles3 v

--- android/src/main/cpp/translated_opengles.cpp
+++ android/src/main/cpp/translated_opengles.cpp
@@ -9,13 +9,13 @@
 #include <GLES3/gl3.h>
 #endif
 
-void tgf_gles::clearcolormask(unsigned int m, float r, float g, float b, float a) override {
+void tgf_gles::clearcolormask(unsigned int m, float r, float g, float b, float a) {
 		glClearColor(r, g, b, a);
 		glClear(m);
 }
 
 
-void tgf_gles::viewport(unsigned int x, unsigned int y, unsigned int w, unsigned int h) override {
+void tgf_gles::viewport(unsigned int x, unsigned int y, unsigned int w, unsigned int h) {
 		glViewport(x, y, w, h);
 }
 
--- android/src/main/cpp/translated_opengles.h
+++ android/src/main/cpp/translated_opengles.h
@@ -6,8 +6,8 @@
 class tgf_gles : public TranslatedGraphicsFunction {
 public:
 	~tgf_gles() override {}
-	void clearcolormask(unsigned int, float, float, float, float) override;
-	void viewport(unsigned int, unsigned int, unsigned int, unsigned int) override;
+	void clearcolormask(unsigned int, float, float, float, float);
+	void viewport(unsigned int, unsigned int, unsigned int, unsigned int);
 };
 
 #endif // Included_TGLES
